56178bddf87e311acc98212d02257d76a9c715b3
danyel salazar <danygasia2@gmail.com>
2026-05-05 12:59:14 -0300

    add healt check route

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,3 +11,8 @@ const PORT = 4000
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`,);
 })
+
+// creo una primera ruta del servidor para probar
+app.get("/", (req, res)=>{
+    res.send("API corriendo")
+})
