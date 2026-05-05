6d584a74acbfd37cb32425b0ed3ee3a67c2e4306
danyel salazar <danygasia2@gmail.com>
2026-05-05 12:48:30 -0300

    feat: initialize backend with express server

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express"
+import coors from "cors"
+
+const app = express()
+
+app.use(coors())
+app.use(express.json())
+
+const PORT = 4000
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`,);
+})
